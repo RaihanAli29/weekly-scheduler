f41f57501904b3d077340f33503bbfb1ebe9381a
RaihanAli29 <149912064+RaihanAli29@users.noreply.github.com>
2024-01-06 21:59:40 +0000

    Upload ReadMe file

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# weekly-scheduler
+# Weekly Scheduler
+
+## Table of Contents
+
+- [Description](#description)
+- [Preview](#preview)
+- [Usage](#usage)
+- [Languages](#languages)
+- [Notes](#Notes)
+- [Deployment](#deployment)
+- [Resources](#resources)
+
+## Description
+
 Developed a daily planner enabling users to manage their schedule from 9am to 9pm, tracking their availability.
+
+## Preview
+
+![Weekly Scheduler Preview](./images/05-third-party-apis-homework-demo.gif)
+
+## Usage
+- Open Weekly Scheduler in browser of choice. 
+- Select hour.
+- Input details to pertaining to the hour. 
+- Save by clicking on the icon on the right. 
+- Repeat as neccassary.
+- Refresh the page and your inputs will still be available to view. 
+
+## Languages
+- JavaScript
+- jQuery
+- HTML
+- CSS
+- GitHub
+
+# Notes
+
+```
+ for (var hour = 9; hour <= 21; hour++)
+```
+
+I used a loop to create time blocks for each hour from 9 AM to 9 PM. 
+
+```
+$("<textarea>").addClass("col event " + (isPast ? "past" : isPresent ? "present" : "future")),
+```
+I added a conditional (ternary) operator wilst adding classes to selected textarea elements, the styling of the selected elements change according to the CSS and whether the hour is in the past, present or future. 
+
+# Resources
+- Conditional (ternary) operator [- read more...](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator)
+- Format (day.js) [- read more...](https://day.js.org/docs/en/display/format)
+- this and $(this) in jQuery callbacks [- read more...](https://coding.abel.nu/2013/04/this-and-this-in-jquery-callbacks/)
+- .val() method [- read more...](https://www.w3schools.com/jquery/html_val.asp)
+- contains() selector [- read more...](https://www.w3schools.com/jquery/sel_contains.asp)
+
+# Acknowledgements
--- starter/script.js
+++ starter/script.js
@@ -1,37 +1,47 @@
+// When the document is ready
 $(document).ready(function() {
   // Display the current day at the top of the calendar
-  $("#currentDay").text(dayjs().format('dddd, MMM D, YYYY'));
+  var currentDate = dayjs().format('dddd, MMM D, YYYY');
+  $("#currentDay").text(currentDate);
 
   // Clear the container before adding new time blocks
   $(".container").empty();
 
-  // Present timeblocks for standard business hours
+  // Create time blocks for each hour from 9AM to 9PM
   for (var hour = 9; hour <= 21; hour++) {
+    // Create a time label for the current hour
     var timeLabel = dayjs().hour(hour).minute(0).format('h A');
+
+    // Determine if the hour is in the past, present, or future
     var isPast = hour < dayjs().hour();
     var isPresent = hour === dayjs().hour();
-    var timeRow = $("<div>").addClass("row time-block");
-    timeRow.append(
+
+    // Create a new time block
+    var timeBlock = $("<div>").addClass("row time-block");
+    timeBlock.append(
       $("<div>").addClass("col-2 hour").text(timeLabel),
       $("<textarea>").addClass("col event " + (isPast ? "past" : isPresent ? "present" : "future")),
       $("<button>").addClass("col-1 saveBtn").html('<i class="far fa-save"></i>')
     );
-    $(".container").append(timeRow);
+
+    // Add the time block to the container
+    $(".container").append(timeBlock);
   }
 
-  // Allow a user to enter an event when they click a timeblock
+  // Allow a user to enter an event when they click a time block
   $(document).on("click", ".event", function() {
     $(this).addClass("active");
   });
 
   // Save the event in local storage when the save button is clicked
   $(document).on("click", ".saveBtn", function() {
-    var eventText = $(this).siblings(".event").val();
-    var eventHour = $(this).siblings(".hour").text();
+    var eventText = $(this).parent().children(".event").val();
+    var eventHour = $(this).parent().children(".hour").text();
     localStorage.setItem(eventHour, eventText);
   });
+  
 
-  // Persist events between refreshes of a page
+  // Retrieve and display saved events from local storage
   for (var hour = 9; hour <= 21; hour++) {
     var savedEvent = localStorage.getItem(dayjs().hour(hour).minute(0).format('h A'));
     if (savedEvent) {
